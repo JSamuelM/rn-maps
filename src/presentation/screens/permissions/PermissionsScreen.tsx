85e737b9fa4224f77458932daa1e51342b803d6b
Samuel Mena <samuel23mena@gmail.com>
2024-06-22 17:42:44 -0600

    :sparkles: Location Permission

--- src/presentation/screens/permissions/PermissionsScreen.tsx
+++ src/presentation/screens/permissions/PermissionsScreen.tsx
@@ -1,9 +1,21 @@
-import { View, Text } from 'react-native';
+import { View, Text, Pressable } from 'react-native';
+import { globalStyles } from '../../../config/theme/styles';
+import { usePermissionStore } from '../../stores/permissions/usePermissionStore';
 
 export const PermissionsScreen = () => {
+  const { locationStatus, requestLocationPermission } = usePermissionStore();
+
   return (
-    <View>
-      <Text>PermissionsScreen</Text>
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text style={{ color: 'black' }}>Habilitar ubicación</Text>
+
+      <Pressable
+        style={globalStyles.btnPrimary}
+        onPress={requestLocationPermission}>
+        <Text style={{ color: 'white' }}>Habilitar Localización</Text>
+      </Pressable>
+
+      <Text style={{ color: 'black' }}>Estado actual: {locationStatus}</Text>
     </View>
   );
 };
